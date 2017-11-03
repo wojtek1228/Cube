a1bcfa3b905d92c77fd257ffa75b95a2aa2b048d
Wojciech <Wojciech@macbook-pro-wojciech.local>
2017-11-03 20:30:10 +0100

    Initial Commit

---
+++ RubicCube/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RubicCube
+//
+//  Created by Wojciech on 03.11.2017.
+//  Copyright © 2017 Wojciech. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
